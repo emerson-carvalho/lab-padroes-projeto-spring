3c137dec1eb19ec0a4b65ade8204e258880f8110
Emerson Carvalho <emersoncarvalhoss11@outlook.com>
2022-10-23 17:30:44 -0300

    first commit

---
+++ src/main/java/me/dio/gof/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package me.dio.gof.repository;
+
+import me.dio.gof.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
